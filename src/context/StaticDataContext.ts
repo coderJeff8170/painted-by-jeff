585a099fc09e5ec4ac67bc38b1ec8d0e267cb899
Jeff Andrews <babajeff8170@gmail.com>
2024-05-14 17:30:26 -0400

    ensures json server looks to data directory for source of truth

--- src/context/StaticDataContext.ts
+++ src/context/StaticDataContext.ts
@@ -1,5 +1,6 @@
 import { createContext } from "react";
-import file from "../../db.json";
+// import file from "../../db.json";
+import file from "../data/artwork.json";
 
 
 export const StaticDataContext = createContext(file);
