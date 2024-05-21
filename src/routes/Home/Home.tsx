2615aa74aa9187c1a5fa721ef84e665dbbb3710e
Jeff Andrews <babajeff8170@gmail.com>
2024-05-21 12:34:06 -0400

    adds background image to splash page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -4,6 +4,7 @@ import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
   const headerSuperTitle = <h4>Welcome to the new and improved</h4>;
@@ -12,8 +13,11 @@ const Home: React.FC = () => {
 
   return (
     <>
-    <Container fluid>
+    <Container className={styles.demoWrap} fluid>
       {/* <Container fluid style={{ height: "100%", backgroundImage: "url(/FullSize/MyFaveMemoryPlaceholder.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", opacity: "0.33" }}> */}
+        {/* <Image className={styles.demoBg} src="FullSize/MyFaveMemoryPlaceholder.png" /> */}
+        <div className={styles.demoBg}></div>
+        <div className={styles.demoContent}>
         <Row>
           <Col>
             <Header
@@ -24,7 +28,7 @@ const Home: React.FC = () => {
             />
           </Col>
         </Row>
-        <Row style={{ height: "500px" }}>
+        <Row style={{ height: "500px" }} className="mb-5">
           <Col className="text-center" style={{ height: "100%" }}>
             <Link to="/Analog">
               <Image
@@ -37,6 +41,7 @@ const Home: React.FC = () => {
             </Link>
           </Col>
         </Row>
+        </div>
       </Container>
     </>
   );
